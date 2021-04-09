f3b6f6ee32c9c5c3613327bc5dca52f7c50f3c12
shyam <shyam.sodhani@spit.ac.in>
2021-04-09 11:46:10 +0530

    local url to azure

--- src/app/utility/app.init.ts
+++ src/app/utility/app.init.ts
@@ -4,7 +4,7 @@ export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boo
     return () =>
         keycloak.init({
             config: {
-                url: 'http://localhost:8080/auth',
+                url: 'https://keycloakserver3.azurewebsites.net/auth',
                 realm: 'Testing',
                 clientId: 'Demo',
             },
