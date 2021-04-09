2012229dd6c8668e2c8384090205c174caf11c42
shyam <shyam.sodhani@spit.ac.in>
2021-04-09 15:08:16 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { initializeKeycloak } from '../app/utility/app.init';
 import { TrainingComponent } from './training/training.component';
 import { QueryComponent } from './query/query.component';
 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,6 +31,7 @@ import { QueryComponent } from './query/query.component';
       multi: true,
       deps: [KeycloakService],
     },
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
   ],
   bootstrap: [AppComponent]
 })
